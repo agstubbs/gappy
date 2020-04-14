bd69d23be1c52e377d908d4e0b29ffddf66aff6b
Andrew Stubbs <andy@andystubbs.com>
2020-04-14 23:30:25 +0100

    add logic to swap authz code for access and refresh tokens

--- src/gappy/oauth2.clj
+++ src/gappy/oauth2.clj
@@ -20,13 +20,13 @@
           char
           (.encode (.withoutPadding (Base64/getUrlEncoder)) buffer))))
 
-(defn build-auth-url [{:keys
+(defn build-authn-url [{:keys
                       [auth_uri scope response_type redirect_uri]
-                      :as auth-data
+                      :as authn-data
                       :or {response_type "code"
                            redirect_uri "urn:ietf:wg:oauth:2.0:oob"}}]
   (let [scope-str (s/join " " scope)
-        params (conj (select-keys auth-data [:client_id
+        params (conj (select-keys authn-data [:client_id
                                              :response_type
                                              :code_challenge
                                              :code_challenge_method
@@ -44,6 +44,37 @@
                                "="
                                (URLEncoder/encode v)))) [] params)))
     ))
+
+(defn build-authz-url [{:keys
+                        [token_uri
+                         client_id client_secret
+                         code code_verifier
+                         grant_type
+                         redirect_uri]
+                        :as authz-data
+                        :or {redirect_uri "urn:ietf:wg:oauth:2.0:oob"
+                             grant_type "authorization_code"}}]
+  (let [params (conj (select-keys authz-data [:client_id
+                                              :client_secret
+                                              :code
+                                              :code_verifier])
+                     {:redirect_uri redirect_uri
+                      :grant_type grant_type})]
+    (str token_uri "?"
+         (s/join "&"
+                 (reduce-kv
+                  (fn [c k v]
+                    (conj c
+                          (str (URLEncoder/encode (name k))
+                               "="
+                               (URLEncoder/encode v)))) [] params)))
+    ))
+;; client_id 	The client ID obtained from the API Console Credentials page.
+;; client_secret 	The client secret obtained from the API Console Credentials page.
+;; code 	The authorization code returned from the initial request.
+;; code_verifier 	The code verifier you created in Step 1.
+;; grant_type 	As defined in the OAuth 2.0 specification, this field must contain a value of authorization_code.
+;; redirect_uri 	One of the redirect URIs listed for your project in the API Console Credentials page for the given client_id.
 
 ;; https://developers.google.com/identity/protocols/oauth2/openid-connect#createxsrftoken
 (defn generate-state []
